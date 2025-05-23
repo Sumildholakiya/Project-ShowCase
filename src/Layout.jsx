7260b3160a95ad77ca9fb8b453a2ca3d44f3f488
BroCodeAgency <brocodeagency7@gmail.com>
2025-05-23 09:06:18 +0530

    updated

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom'
-import Header from './components/Header/header'
+import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 
 const Layout = () => {
